ccd77c12c69edbfb2b4895a47f5e7b789b87b8d3
Tyler <tyler001crawford@gmail.com>
2023-08-05 19:09:39 -0400

    removed unused imports

--- scraperModule.py
+++ scraperModule.py
@@ -1,11 +1,9 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
 
 class StudyRoomBooker:
     def __init__(self):
